9b0afab62b3026b803b6f14d90f0823a4ee3c64c
iamraghav <raghav3992@gmail.com>
2018-02-19 22:10:55 +0530

    added continue functionality in question area

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -12,6 +12,38 @@
 
       vm.quizMetrics = quizMetrics;
       vm.dataService = DataService;
+      vm.questionAnswered = questionAnswered;
+      vm.setActiveQuestion =setActiveQuestion;
+      vm.activeQuestion = 0;
+
+      var numQuestionAnswered = 0;
+
+      function setActiveQuestion() {
+        var breakOut = false;
+        var quizLength = DataService.quizQuestions.length - 1;
+
+        while(!breakOut) {
+          vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
+
+          if(DataService.quizQuestions[vm.activeQuestion].selected === null) {
+            breakOut = true;
+          }
+        }
+      }
+
+      function questionAnswered() {
+
+        if(DataService.quizQuestions[vm.activeQuestion].selected !== null) {
+
+          var quizLength = DataService.quizQuestions.length;
+          numQuestionAnswered++;
+          if(numQuestionAnswered >= quizLength) {
+            //finalize quiz
+
+          }
+        }
+        vm.setActiveQuestion();
+      }
     }
 
 })();
